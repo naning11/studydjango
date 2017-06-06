d0c948d75c123b793e3886d8a756c10e08aeaa74
naning11 <573504672@qq.com>
2017-06-06 19:58:09 +0800

    Add files via upload

---
+++ ls/lsonly/views.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+from django.template.loader import get_template
+from django.template import Context
+from django.http import HttpResponse
+import datetime
